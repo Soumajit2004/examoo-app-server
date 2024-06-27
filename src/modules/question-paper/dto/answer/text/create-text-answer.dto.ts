fc1f918fd7521c9cd34b91d1a82e7bb2d663e778
Soumajit Ghosh <soumojitghosh02@gmail.com>
2024-06-27 14:13:44 +0530

    reverted answer framework

--- src/modules/question-paper/dto/answer/text/create-text-answer.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
-
-export class CreateTextAnswerDto {
-  @IsNotEmpty()
-  @IsString()
-  @MinLength(4)
-  correctText: string;
-}
--- src/modules/question-paper/question-paper.module.ts
+++ src/modules/question-paper/question-paper.module.ts
@@ -6,46 +6,21 @@ import { QuestionPaperController } from './controllers/question-paper.controller
 import { QuestionController } from './controllers/question.controller';
 import { QuestionService } from './services/question.service';
 import { Question } from './entites/question.entity';
-import { McqAnswer } from './entites/answers/mcq/mcq-answer.entity';
-import { TextAnswer } from './entites/answers/text/text-answer.entity';
-import { NumericalAnswer } from './entites/answers/numerical/numerical-answer.entity';
-import { McqOption } from './entites/answers/mcq/mcq-option.entity';
-import { McqResponse } from './entites/answers/mcq/mcq-response.entity';
-import { TextResponse } from './entites/answers/text/text-response.entity';
-import { NumericalResponse } from './entites/answers/numerical/numerical-response.entity';
 import { QuestionPaper } from './entites/question-paper.entity';
 import { QuestionPaperRepository } from '../../database/repositories/question-paper.repository';
 import { QuestionRepository } from '../../database/repositories/question.repository';
 import { AuthModule } from '../auth/auth.module';
-import { AnswerController } from './controllers/answer.controller';
 import { QuestionPaperAccessControlService } from './services/question-paper-access-control.service';
-import { AnswerService } from './services/answer.service';
-import { TextAnswerRepository } from '../../database/repositories/answer/text-answer.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      QuestionPaper,
-      Question,
-      McqAnswer,
-      McqResponse,
-      McqOption,
-      TextAnswer,
-      TextResponse,
-      NumericalAnswer,
-      NumericalResponse,
-    ]),
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forFeature([QuestionPaper, Question]), AuthModule],
   providers: [
     QuestionPaperService,
     QuestionService,
     QuestionPaperRepository,
     QuestionRepository,
     QuestionPaperAccessControlService,
-    AnswerService,
-    TextAnswerRepository,
   ],
-  controllers: [QuestionPaperController, QuestionController, AnswerController],
+  controllers: [QuestionPaperController, QuestionController],
 })
 export class QuestionPaperModule {}
